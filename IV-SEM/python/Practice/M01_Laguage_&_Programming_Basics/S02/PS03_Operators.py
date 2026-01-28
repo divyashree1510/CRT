9911dc0c5eb11e2209596b5b7a6fffc3b7713ac6
divyashree1510 <divyashreejanige@gmail.com>
2026-01-28 11:42:05 +0530

    msg1

---
+++ IV-SEM/python/Practice/M01_Laguage_&_Programming_Basics/S02/PS03_Operators.py
@@ -0,0 +1,17 @@
+x = int(input("Enter a value:"))
+print(x << 2)
+print(x >> 2)
+print(~x)
+#membership operator
+s = input()
+print("on" in s)
+#identity operator ==> is,is not
+x = 10
+y = 20 
+z = 10
+print(x is y)
+print(x is z)
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3]
+print(l1 is l2)
